11a55a0e5f3dbc036d0481a7a9c28f11f15372a8
Anthony <13015521+darknoon29@users.noreply.github.com>
2024-11-21 10:55:10 +0100

    Update server.js
    More Ip Logs

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const port = 1433;
 // Create HTTP server for port 1433
 app.get('/', (req, res) => {
   let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+    console.log(ip);
     if (ip.includes(':')) {
     ip = ip.split(',').find(ip => ip.includes('.'));
   }
